64f9dfe09a95954f89a461b3d1ba4d30ce47218b
Jad Jonaidi <jadjon@umich.edu>
2024-04-20 02:23:52 -0400

    c

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -13,10 +13,10 @@ public class testSelectionSort {
         arr[1] = 9;
         arr[2] = 7;
         arr[3] = 10;
-        arr[4] = 5;
+        arr[4] = 3;
         
         int[] Sortedarr = new int[5];
-        Sortedarr[0] = 5;
+        Sortedarr[0] = 3;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
@@ -37,14 +37,14 @@ public class testSelectionSort {
         arr[1] = -9;
         arr[2] = -7;
         arr[3] = -11;
-        arr[4] = -2;
+        arr[4] = -3;
         
         int[] Sortedarr = new int[5];
         Sortedarr[0] = -11;
         Sortedarr[1] = -9;
         Sortedarr[2] = -8;
         Sortedarr[3] = -7;
-        Sortedarr[4] = -2;
+        Sortedarr[4] = -3;
         SelectionSort sorter = new SelectionSort();
         arr = sorter.basicSelectionSort(arr);
         
